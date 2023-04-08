70ee1574e937c7f4a9de5478715a458ca1080131
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-08 16:51:40 +0200

    NofFound route changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "pages/Home";
 import Movies from "pages/Movies";
-import NotFound from "pages/NofFound";
+import NotFound from "pages/NotFound";
 import { Layout } from "./Layout/Layout";
 
 export const App = () => {
@@ -11,8 +11,8 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
-      </Route>
       <Route path="*" element={<NotFound />} />
+      </Route>
    </Routes>
   );
 };
