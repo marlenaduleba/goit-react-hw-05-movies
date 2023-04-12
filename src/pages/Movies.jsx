9e9f5de8264eb44c3c985a6329eb275495e556b0
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-12 09:44:43 +0200

    unnecessary hook deleted

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { api } from 'service/api';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
