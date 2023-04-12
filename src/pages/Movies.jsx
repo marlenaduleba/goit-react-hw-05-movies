307827551e0c4fea02df3e55cf78828739febf83
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-12 09:40:09 +0200

    searchParams added

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,60 @@
- const Movies = () => {
-    return <div>Movies</div>
-}
+import { useState, useEffect } from 'react';
+import { api } from 'service/api';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+
+export const Movies = () => {
+  const [films, setFilms] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('name') ?? '';
+
+  const location = useLocation();
+
+  const handleInputChange = e => {
+    const name = e.target.value;
+
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+  };
+
+  const handleFromSubmit = e => {
+    e.preventDefault();
+    api.fetchFilmByName(query).then(data => {
+      setFilms(data);
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFromSubmit}>
+        <label>
+          <input type="text" value={query} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {films.length > 0 &&
+          films.map(({ id, title, poster_path }) => (
+            <li key={id}>
+              <Link
+                to={`/movies/${id}`}
+                state={{ from: location }}
+                style={{ display: 'flex' }}
+              >
+                <img
+                  width="50"
+                  src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                  alt={title}
+                />
+
+                <p>{title}</p>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Movies;
