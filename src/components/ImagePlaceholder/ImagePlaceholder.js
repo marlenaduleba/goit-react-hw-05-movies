54e4eacbcfd6816da3db940da41a5bb23d3af0eb
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-12 11:14:18 +0200

    path fixed with https

--- src/components/ImagePlaceholder/ImagePlaceholder.js
+++ src/components/ImagePlaceholder/ImagePlaceholder.js
@@ -2,6 +2,10 @@ export const ImagePlaceholder = pathImage => {
     if (!pathImage) {
       return 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
     }
+
+    if (pathImage.includes('https')) {
+        return pathImage.slice(1, pathImage.length);
+      }
   
     if (pathImage[0] === '/') {
       return `https://image.tmdb.org/t/p/w500${pathImage}`;
