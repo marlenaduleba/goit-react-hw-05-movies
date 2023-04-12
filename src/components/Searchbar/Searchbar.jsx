5dd212eeb69d2821681b5df6de819f094c1772ba
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-12 12:46:29 +0200

    Searchbar added

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import { useSearchParams } from 'react-router-dom';
+
+// import PropTypes from 'prop-types';
+
+import {
+  SearchForm,
+  SearchFormButton,
+  SearchFormButtonLabel,
+  SearchFormInput,
+} from './Searchbar.styled';
+
+export const Searchbar = ({ onSubmit }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('name') ?? '';
+
+  const handleInputChange = e => {
+    const name = e.target.value;
+    //   onChange(e.target.value);
+
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    onSubmit(query);
+  };
+
+  return (
+    <SearchForm onSubmit={handleFormSubmit}>
+      <SearchFormButton type="submit">
+        <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+      </SearchFormButton>
+
+      <SearchFormInput
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movies"
+        value={query}
+        onChange={handleInputChange}
+      />
+    </SearchForm>
+  );
+};
+
+// Searchbar.propTypes = {};
---
+++ src/components/Searchbar/Searchbar.styled.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 600px;
+  background-color: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+`;
+
+export const SearchFormButton = styled.button`
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-image: url('https://cdn-icons-png.flaticon.com/512/1086/1086933.png');
+  background-size: 40%;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const SearchFormButtonLabel = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  clip-path: inset(50%);
+  border: 0;
+`;
+
+export const SearchFormInput = styled.input`
+  display: inline-block;
+  width: 100%;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  padding-left: 4px;
+  padding-right: 4px;
+  &::placeholder {
+    font: inherit;
+    font-size: 18px;
+  }
+`;
