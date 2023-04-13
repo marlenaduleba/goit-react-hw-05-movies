5a5e0bdfa9c3530adf4085ce8a245434af8af8e6
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-13 12:00:12 +0200

    margins fixed

--- src/components/Movie/Movie.styled.js
+++ src/components/Movie/Movie.styled.js
@@ -23,6 +23,7 @@ export const List = styled.ul`
   justify-content: center;
   flex-wrap: wrap;
   gap: 16px;
+  margin-top: 20px;
 `;
 
 export const Item = styled.li`
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  margin-bottom: 16px;
+  /* margin-bottom: 16px; */
   padding: 10px;
   border-radius: 5px;
   color: white;
